9c5593a8292f6861271f1789159589c375bd2839
elygo <el.uzb93@gmail.com>
2020-10-15 22:03:07 +0500

    init: add function reverse_int
    The function reverses the given integer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  var numString = n.toString();
+  if (numString.charAt(0) == '-'){
+    numString = numString.substring(1).split("").reverse().join("")
+  } else {
+    numString = numString.split("").reverse().join("")
+  }
+  return parseInt(numString)
 }
